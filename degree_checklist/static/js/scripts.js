b6018a80095c3161e6e6d3819119f3b6bb0dc179
Jom123410 <57498419+Jom123410@users.noreply.github.com>
2023-10-22 23:41:27 -0500

    Delete degree_checklist directory

--- degree_checklist/static/js/scripts.js
+++
@@ -1,8 +0,0 @@
-function toggleCourses(semester) {
-    const coursesList = document.getElementById(`courses-${semester}`);
-    if (coursesList.style.display === "none") {
-        coursesList.style.display = "block";
-    } else {
-        coursesList.style.display = "none";
-    }
-}
